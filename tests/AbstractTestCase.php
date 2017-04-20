6b7dcc8653e6c3260dc41c4ab5434b90782b1047
Brian Faust <faustbrian@users.noreply.github.com>
2017-04-20 14:04:42 +0300

    Apply fixes from StyleCI (#1)
    [ci skip] [skip ci]

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Invoice.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Tests\Invoice;
 
 use BrianFaust\Invoice\InvoiceServiceProvider;
