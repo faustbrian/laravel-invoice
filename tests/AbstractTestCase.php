fcbb0ae7d83efd806134a0cfb3df2ed195db2889
Brian Faust <hello@brianfaust.de>
2017-04-22 08:47:06 +0300

    Try to fix the mess StyleCI makes

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,14 +2,7 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Invoice.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+
 
 namespace BrianFaust\Tests\Invoice;
 
